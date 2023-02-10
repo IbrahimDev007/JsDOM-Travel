d8f3b43378d517b1b47f07288f0ede79843fce1d
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-10 18:35:05 +0600

    js dom selection

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+// test to connect 
+console.log("helllo js");
+// document Object Model check
+console.log(document);
+// selection in js dome 
+// slection by tag id class for usuaally html 
+
+console.log(document.getElementsByTagName("section"));
+console.log(document.getElementById("plaaces-id-1"));
+console.log(document.getElementsByClassName("places"));
+console.log(document.querySelector("places"));
+console.log(document.querySelector("places"));
+console.log(document.querySelector("section"));
